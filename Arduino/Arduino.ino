cb77ffdf47316def338fcb1fe47bc2795dee3c14
Tijmen34 <23655598+Tijmen34@users.noreply.github.com>
2018-10-05 17:51:27 +0200

    Added BMP280 sensor. Serial prints out the sensor data in Json Object

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -6,50 +6,77 @@
 OneWire oneWire(DS18B20PIN);
 DallasTemperature sensors(&oneWire);
 /*************************/
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
 
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11 
+#define BMP_CS 10
+
+//Adafruit_BMP280 bme;
+Adafruit_BMP280 bme(BMP_CS); //Using SPI right here
 /*************************/
 #include "DHT.h" //Include DHT lib
 #define DHTPIN 7 //Define the pin output for the DHT
 #define DHTTYPE DHT11 //Defines the DHT you use
+
 //Pin 1 - 5v, pin 2 - DHTPIN, pin 4 - GND
 //A 10k resistor has to be put between pin 1 and pin 2
 
 DHT dht(DHTPIN, DHTTYPE);
 /*************************/
 
+#include <ArduinoJson.h>
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Start!");
-  
-  initializeDHT();
+
+  initializeSensors();
 }
 
 void loop() {
   int DHTHumidity = DHTReadHumidity();
   int DHTTemperature = DHTReadTemp();
-  double DS18B20Temperature = DS18B20ReadTemp();
-
-  String humidity1 = "Humidity: ";
-  String humidity2 = " %\t";
-  String humidityString = humidity1 + DHTHumidity + humidity2;
-
-  String temperature1 = "DHT Temperature: ";
-  String temperature2 = " *C\n";
-  String temperatureString = temperature1 + DHTTemperature + temperature2;
-
-  String temperature3 = "DS18B20 Temperature: ";
-  String temperatureString2 = temperature3 + DS18B20Temperature + temperature2;
   
-  Serial.print(humidityString);
-  Serial.print(temperatureString);
-  Serial.print(temperatureString2);
-  delay(2000);
+  double DS18B20Temperature = DS18B20ReadTemp();
+  
+  double BMP280Temperature = bme.readTemperature();
+  float BMP280Pressure = bme.readPressure();
+  double BMP280Altitude = bme.readAltitude(1030.00);
+
+  StaticJsonBuffer<200> jsonBuffer;
+
+  JsonObject& tempData = jsonBuffer.createObject();
+
+  tempData["ds18b20"] = DS18B20Temperature;
+
+  JsonObject& dht11 = tempData.createNestedObject("dht11");
+  dht11["temperature"] = DHTTemperature;
+  dht11["humidity"] = DHTHumidity;
+
+  JsonObject& bmp280 = tempData.createNestedObject("bmp280");
+  bmp280["temperature"] = BMP280Temperature;
+  bmp280["pressure"] = BMP280Pressure;
+  bmp280["altitude"] = BMP280Altitude;
+
+  tempData.prettyPrintTo(Serial);
+  
+  delay(5000);
 }
 
 
 
-void initializeDHT() {
+void initializeSensors() {
+  if(!bme.begin()){
+    Serial.println("Could not find BMP280 sensor!");
+  } else {
   dht.begin();
+  bme.begin();
+  }
 }
 
 int DHTReadTemp() {
