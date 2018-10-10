70005962757027dae8b41d8211cd440c1d5705ea
Tijmen34 <23655598+Tijmen34@users.noreply.github.com>
2018-10-10 22:41:01 +0200

    Changed some formatting

--- Python/ArduinoToMySQL.py
+++ Python/ArduinoToMySQL.py
@@ -9,16 +9,17 @@ import serial
 db = mysql.connector.connect(host="localhost",
                              user="",
                              password="",
-                             db="iot")
+                             db="internetofthings")
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 time.sleep(0.5)
 
 requestparams = {'q': '', 'APPID': ''}
+openweatherurl = 'http://api.openweathermap.org/data/2.5/weather'
 
 
 def insertsensordata(sensorJson):
-    request = requests.get('http://api.openweathermap.org/data/2.5/weather', params=requestparams)
+    request = requests.get(openweatherurl, params=requestparams)
     requestData = request.json()
     cursor = db.cursor()
 
@@ -30,9 +31,9 @@ def insertsensordata(sensorJson):
             cursor.execute("INSERT INTO dht11 (temperature, humidity) VALUES (%s, %s)",
                            (sensorJson["dht11"]["temperature"], sensorJson["dht11"]["humidity"]))
             cursor.execute("INSERT INTO ds18b20 (temperature) VALUES (%s)",
-                           (sensorJson["ds18b20"],))
+                           (("{:.2f}".format(sensorJson["ds18b20"])),))
             cursor.execute("INSERT INTO openweather (temperature, pressure, humidity) VALUES (%s, %s, %s)",
-                           (requestData["main"]["temp"] - 273.15, requestData["main"]["pressure"],
+                           (("{:.2f}".format(requestData["main"]["temp"] - 273.15)), requestData["main"]["pressure"],
                             requestData["main"]["humidity"]))
             db.commit()
             print("Data inserted at ", datetime.datetime.now())
