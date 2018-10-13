bd153239d903213ea12c5308e3e2de3955441d09
Tijmen34 <23655598+Tijmen34@users.noreply.github.com>
2018-10-13 18:03:14 +0200

    Start making 2 api get calls

--- Service/src/controllers/weatherDataController.ts
+++ Service/src/controllers/weatherDataController.ts
@@ -3,22 +3,9 @@ import connection from '../databases/database'
 
 export const WeatherDataController = {
   getByDates: function(request, response) {
-    connection.getConnection((err, conn) => {
-      if (err) {
-        response.json(err);
-      } else {
-        conn.query("SELECT * FROM bmp280 WHERE id = 1", (err, rows) => {
-          if (err) {
-            response.json(err)
-          } else {
-            response.json(rows)
-          }
-        })
-      }
-    })
-  },
-
-  getValue: function(request, response) {
+    const startDate = request.params.startDate
+    const endDate = request.params.endDate
+    console.log(startDate, endDate)
     connection.getConnection((err, conn) => {
       if (err) {
         response.json(err);
@@ -49,6 +36,49 @@ export const WeatherDataController = {
           }
         })
         conn.query("SELECT * FROM openweather WHERE timestamp BETWEEN '2018-10-10 22:00:00' AND '2018-10-10 23:00:00'", (err, rows, fields) => {
+          if (err) {
+            response.json(err)
+          } else {
+            openweather = {}
+            openweather[fields[0].table] = rows
+            response.json([bmp280, ds18b20, dht11, openweather])
+          }
+        })
+      }
+    })
+  },
+
+  getAllData: function(request, response) {
+    connection.getConnection((err, conn) => {
+      if (err) {
+        response.json(err);
+      } else {
+        let bmp280, dht11, ds18b20, openweather;
+        conn.query("SELECT * FROM bmp280", (err, rows, fields) => {
+          if (err) {
+            response.json(err)
+          } else {
+            bmp280 = {}
+            bmp280[fields[0].table] = rows
+          }
+        })
+        conn.query("SELECT * FROM dht11", (err, rows, fields) => {
+          if (err) {
+            response.json(err)
+          } else {
+            dht11 = {}
+            dht11[fields[0].table] = rows
+          }
+        })
+        conn.query("SELECT * FROM ds18b20", (err, rows, fields) => {
+          if (err) {
+            response.json(err)
+          } else {
+            ds18b20 = {}
+            ds18b20[fields[0].table] = rows
+          }
+        })
+        conn.query("SELECT * FROM openweather", (err, rows, fields) => {
           if (err) {
             response.json(err)
           } else {
--- Service/src/routes/weatherDataRoutes.ts
+++ Service/src/routes/weatherDataRoutes.ts
@@ -1,10 +1,8 @@
-import {Request, Response} from "express";
 import { WeatherDataController } from "../controllers/weatherDataController"
 
 export class Routes {
   public routes(app): void {
-    app.route('/').get(WeatherDataController.getValue)
-
+    app.route('/data/getall').get(WeatherDataController.getAllData)
     app.route('/data/:startDate&:endDate').get(WeatherDataController.getByDates)
     }
 }
