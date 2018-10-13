244fc5db7040bc97d28378831417ac6154e1eaae
Tijmen34 <23655598+Tijmen34@users.noreply.github.com>
2018-10-13 15:17:07 +0200

    Get from all tables with tablename

--- Service/src/controllers/weatherDataController.ts
+++ Service/src/controllers/weatherDataController.ts
@@ -4,7 +4,7 @@ import connection from '../databases/database'
 export const WeatherDataController = {
   getByDates: function(request, response) {
     connection.getConnection((err, conn) => {
-      if (err){
+      if (err) {
         response.json(err);
       } else {
         conn.query("SELECT * FROM bmp280 WHERE id = 1", (err, rows) => {
@@ -20,18 +20,41 @@ export const WeatherDataController = {
 
   getValue: function(request, response) {
     connection.getConnection((err, conn) => {
-      if (err){
+      if (err) {
         response.json(err);
       } else {
-        conn.query("SELECT * FROM bmp280 WHERE timestamp BETWEEN '2018-10-10 22:00:00' AND '2018-10-10 23:00:00';" +
-        "SELECT * FROM dht11 WHERE timestamp BETWEEN '2018-10-10 22:00:00' AND '2018-10-10 23:00:00';" +
-        "SELECT * FROM openweather WHERE timestamp BETWEEN '2018-10-10 22:00:00' AND '2018-10-10 23:00:00';" +
-        "SELECT * FROM ds18b20 WHERE timestamp BETWEEN '2018-10-10 22:00:00' AND '2018-10-10 23:00:00';"
-        , (err, rows) => {
+        let bmp280, dht11, ds18b20, openweather;
+        conn.query("SELECT * FROM bmp280 WHERE timestamp BETWEEN '2018-10-10 22:00:00' AND '2018-10-10 23:00:00'", (err, rows, fields) => {
           if (err) {
             response.json(err)
           } else {
-            response.json(rows)
+            bmp280 = {}
+            bmp280[fields[0].name] = rows
+          }
+        })
+        conn.query("SELECT * FROM dht11 WHERE timestamp BETWEEN '2018-10-10 22:00:00' AND '2018-10-10 23:00:00'", (err, rows, fields) => {
+          if (err) {
+            response.json(err)
+          } else {
+            dht11 = {}
+            dht11[fields[0].name] = rows
+          }
+        })
+        conn.query("SELECT * FROM ds18b20 WHERE timestamp BETWEEN '2018-10-10 22:00:00' AND '2018-10-10 23:00:00'", (err, rows, fields) => {
+          if (err) {
+            response.json(err)
+          } else {
+            ds18b20 = {}
+            ds18b20[fields[0].name] = rows
+          }
+        })
+        conn.query("SELECT * FROM openweather WHERE timestamp BETWEEN '2018-10-10 22:00:00' AND '2018-10-10 23:00:00'", (err, rows, fields) => {
+          if (err) {
+            response.json(err)
+          } else {
+            openweather = {}
+            openweather[fields[0].name] = rows
+            response.json(bmp280, ds18b20, dht11, openweather)
           }
         })
       }
