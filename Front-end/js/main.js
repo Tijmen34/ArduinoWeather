03e08264e99b3d3e0f4bda7d3a9f844fece830b4
Tijmen Stor <tijmen.stor@ahold.com>
2018-10-27 00:36:32 +0200

    Added weather icons. Start to work towards receiving data.

--- Front-end/js/main.js
+++ Front-end/js/main.js
@@ -1,6 +1,27 @@
+let currentTemperatures= {}
 /**
   * On window load.
   */
 $(function(){
-  
+  loadData();
+})
+
+function loadData() {
+  const currentTime = new Date($.now());
+  const parsedTime = currentTime.getFullYear() + "-" + (currentTime.getMonth()+1) + "-" + currentTime.getDate() + "%20"
+                + currentTime.getHours() + ":"
+                + currentTime.getMinutes() + ":"
+                + currentTime.getSeconds();
+  const parsedTimeShort = currentTime.getFullYear() + "-" + (currentTime.getMonth()+1) + "-" + currentTime.getDate() + "%20"
+                + currentTime.getHours() + ":"
+                + (currentTime.getMinutes() - 1) + ":"
+                + currentTime.getSeconds();
+  const postUrl = "http://192.168.1.32:3000/data/" + parsedTimeShort + "&" + parsedTime;
+
+  console.log(parsedTimeShort, parsedTime, postUrl)
+  // $.post("192.168.1.32:3000/data/")
+}
+
+var vm = new Vue({
+  data: currentTemperatures
 })
