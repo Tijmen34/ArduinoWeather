0b8c4e9476b386e97cdbe3cbf6d2395e6e88b158
Tijmen34 <23655598+Tijmen34@users.noreply.github.com>
2018-10-27 13:40:24 +0200

    Added cors to service.

--- Service/src/server.ts
+++ Service/src/server.ts
@@ -1,6 +1,8 @@
 import app from "./app";
+import * as cors from 'cors';
 const PORT = 3000;
 
+app.use(cors())
 app.listen(PORT, () => {
   console.log('Express server listening on port ' + PORT);
 })
