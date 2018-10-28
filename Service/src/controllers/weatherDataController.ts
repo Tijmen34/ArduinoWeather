23c037be67a9aeb4eae7a89e653d67d5fdce80d4
Tijmen34 <23655598+Tijmen34@users.noreply.github.com>
2018-10-28 21:44:48 +0100

    Added API route for current weather.

--- Service/src/controllers/weatherDataController.ts
+++ Service/src/controllers/weatherDataController.ts
@@ -98,5 +98,52 @@ export const WeatherDataController = {
         })
       }
     })
+  },
+  getCurrentWeather: function (response) {
+    connection.getConnection((err, conn) => {
+      if (err) {
+        response.json(err);
+        return;
+      } else {
+        let bmp280, dht11, ds18b20, openweather;
+        conn.query("SELECT * FROM bmp280 WHERE id = (SELECT MAX(id) FROM bmp280)", (err, rows, fields) => {
+          if (err) {
+            response.json(err)
+            return;
+          } else {
+            bmp280 = {}
+            bmp280[fields[0].table] = rows
+          }
+        })
+        conn.query("SELECT * FROM dht11 WHERE id = (SELECT MAX(id) FROM dht11)", (err, rows, fields) => {
+          if (err) {
+            response.json(err)
+            return;
+          } else {
+            dht11 = {}
+            dht11[fields[0].table] = rows
+          }
+        })
+        conn.query("SELECT * FROM ds18b20 WHERE id = (SELECT MAX(id) FROM ds18b20)", (err, rows, fields) => {
+          if (err) {
+            response.json(err)
+            return;
+          } else {
+            ds18b20 = {}
+            ds18b20[fields[0].table] = rows
+          }
+        })
+        conn.query("SELECT * FROM openweather WHERE id = (SELECT MAX(id) FROM openweather)", (err, rows, fields) => {
+          if (err) {
+            response.json(err)
+            return;
+          } else {
+            openweather = {}
+            openweather[fields[0].table] = rows
+            response.json([bmp280, ds18b20, dht11, openweather])
+          }
+        })
+      }
+    })
   }
 }
