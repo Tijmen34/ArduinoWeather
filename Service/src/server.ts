9a9b0433a270bbde7a4998ae22be7359e6f21dbf
Tijmen34 <23655598+Tijmen34@users.noreply.github.com>
2018-10-30 16:05:07 +0100

    Remove cors

--- Service/src/server.ts
+++ Service/src/server.ts
@@ -1,8 +1,6 @@
 import app from "./app";
-import * as cors from 'cors';
 const PORT = 3000;
 
-app.use(cors())
 app.listen(PORT, () => {
   console.log('Express server listening on port ' + PORT);
 })
