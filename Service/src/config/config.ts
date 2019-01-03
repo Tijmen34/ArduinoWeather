a1a87a32d879f18de67ffca1ba3283cb96fb1f59
Tijmen34 <23655598+Tijmen34@users.noreply.github.com>
2019-01-03 20:55:04 +0100

    Removed unused folders and old stuff. Started on new frontend with Vue.

--- Service/src/config/config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from '../model/config';
-
-export const config: Config = {
-  DB_NAME: "internetofthings",
-  DB_PASSWORD: "iotiscool",
-  DB_USERNAME: "iot"
-};
--- Service/src/model/config.ts
+++
@@ -1,5 +0,0 @@
-export interface Config {
-  DB_NAME: string,
-  DB_PASSWORD: string,
-  DB_USERNAME: string
-}
